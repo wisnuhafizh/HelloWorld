bb2f1de6ef5d45632954d90bf0ed0f8b553f9540
wisnuhafish <wisnuhafizh96@gmail.com>
2025-11-01 10:45:36 +0700

    demo git commit

---
+++ BasicJava.java
@@ -0,0 +1,40 @@
+public class BasicJava {
+    public static void cetakPesan() {
+        System.out.println("Selamat Pagi");
+        System.out.println("Selamat mendapat pekerjaan");
+    }
+
+    public class Candidate{
+        //Instance variable
+        private String firstName;
+        private String lastName;
+        private String party;
+
+        //Methods
+        public void setFirstName(String fname){
+            firstName = fname;
+        }
+        public String getFirstName(){
+            return firstName;
+        }
+    }
+
+    Candidate candidate1 = new Candidate();
+    Candidate candidate3 = new Candidate();
+
+
+    public static void main() {
+        cetakPesan();
+        System.out.println("Hell O World");
+        System.out.println();
+
+        //Statement
+
+        //How to write in Bracket
+
+        //Reformat
+
+        /*ini adalah
+        multiline */
+    }
+}
